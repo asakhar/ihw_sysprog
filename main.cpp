dd134bfd1b12da7b03f90915cafab22c42b3b87b
asakhar <a@sakhar.ru>
2022-03-25 14:47:24 +0300

    evv task impl

--- main.cpp
+++ main.cpp
@@ -1,112 +1,150 @@
 #include <atomic>
+#include <bitset>
+#include <cassert>
 #include <chrono>
-#include <cmath>
-#include <cstdio>
-#include <cwchar>
+#include <concepts>
+#include <cstring>
+#include <functional>
 #include <iostream>
+#include <limits>
 #include <random>
 #include <sstream>
+#include <string>
 #include <thread>
 #include <vector>
 
-#include "errors.hpp"
-#define todo() assert(false && "todo")
-#define loop while (1)
-
 using namespace std::chrono_literals;
 
+#define green "\033[0;32m"
+#define red "\033[0;31m"
+#define reset "\033[0m"
+#define yellow "\033[0;33m"
+
+using string = std::vector<std::uint8_t>;
+
+static int ARGC;
+static char const** ARGV;
+void usage();
+template <typename T>
+T input_checked(char const* string);
+string gen_random_string(size_t const size, std::mt19937& mt);
+bool check_hash(size_t hash, size_t Z);
+template <std::integral T>
+inline void hash_combine(std::size_t& seed, const T& v);
+
+using std::chrono::duration_cast;
+
 int main(int argc, char const* argv[]) {
-  std::locale::global(std::locale("en_US.utf8"));
-  std::wcout.imbue(std::locale());
-  std::wprintf(L"\n");
   ARGC = argc;
   ARGV = argv;
-  if (argc != 3) {
+  if (argc < 3) {
+    std::cerr << red "Invalid number of arguments!\n" reset;
     usage();
   }
-  auto const radius = parse_or_error<double>(argv[1]);
-  auto const radiussq = radius * radius;
-  auto const total = parse_or_error<size_t>(argv[2]);
-  auto const NUM_THREADS = 9;
-  std::atomic<ssize_t> remaining = total;
-  std::atomic<size_t> inside = 0;
+  auto const Z = input_checked<size_t>(argv[1]);
+  auto const N = input_checked<size_t>(argv[2]);
 
-  std::vector<std::thread> threads{};
-  auto const before = std::chrono::high_resolution_clock::now();
   std::random_device rd;
-  for (size_t i = 0; i < NUM_THREADS; ++i) {
-    auto const seed = rd();
-    threads.emplace_back([seed, radius, radiussq, &remaining, &inside]() {
-      auto const BATCH = 10000l;
-      std::mt19937 mt{seed};
-      std::uniform_real_distribution<double> urd{-radius, radius};
-      loop {
-        ssize_t local_remaining = remaining.load(std::memory_order_seq_cst);
-        if (local_remaining == 0) {
-          return;
-        }
-        if (!remaining.compare_exchange_strong(
-                local_remaining, std::max(0l, local_remaining - BATCH),
-                std::memory_order_seq_cst, std::memory_order_relaxed)) {
-          continue;
-        }
-        auto const batch_size = std::min(BATCH, local_remaining);
-        size_t batch_inside = 0;
-        for (ssize_t i = 0; i < batch_size; ++i) {
-          double const magsq = pow(urd(mt), 2) + pow(urd(mt), 2);
-          batch_inside += magsq <= radiussq;
-        }
-        inside.fetch_add(batch_inside, std::memory_order_relaxed);
-      }
-    });
-  }
-  loop {
-    auto const local_remaining = remaining.load(std::memory_order_relaxed);
-    auto const done = (total - local_remaining) * 100 / total;
-    auto const divisor = 100. / 45.;
-    auto const done_3 = done / divisor;
-    std::wstring progress;
-    progress.insert(0, (45 - done_3), L' ');
-    progress.insert(0, done_3, L'\u2588');
-    std::wstring control = L"\u001b[33m";
-    if (done == 100) {
-      control = L"\u001b[32m";
+  std::mt19937 mt{rd()};
+  std::uniform_int_distribution<size_t> urd{20, 1000};
+  string const target_string = gen_random_string(urd(mt), mt);
+  std::vector<std::thread> threads;
+  std::atomic_bool complete = false;
+  std::uint64_t result;
+  std::size_t result_hash;
+
+  auto started_time = std::chrono::high_resolution_clock::now();
+  size_t const hash_checkpoint = [&target_string] {
+    size_t hash_calc = 0;
+    for (auto item : target_string) {
+      hash_combine(hash_calc, item);
     }
-    std::wprintf(L"\r      [ %ls%ls\u001b[0m ] %3lu%%  ", control.c_str(),
-                 progress.c_str(), done);
-    std::wcout.flush();
-    if (local_remaining == 0) {
-      break;
-    }
-    std::this_thread::sleep_for(100ms);
+    return hash_calc;
+  }();
+
+  for (size_t i = 0; i < N; ++i) {
+    auto seed = rd();
+    threads.emplace_back(
+        [hash_checkpoint, seed, Z, &result, &complete, &result_hash]() {
+          thread_local static std::mt19937 mt{seed};
+          std::uniform_int_distribution<uint64_t> uid{
+              0, std::numeric_limits<uint64_t>::max()};
+
+          size_t hash;
+          std::uint64_t attempt;
+          do {
+            hash = hash_checkpoint;
+            attempt = uid(mt);
+            hash_combine(hash, attempt);
+          } while (!check_hash(hash, Z) &&
+                   !complete.load(std::memory_order_relaxed));
+          if (complete.exchange(true, std::memory_order_seq_cst)) return;
+          result = attempt;
+          result_hash = hash;
+        });
   }
-  for (auto& thread : threads) thread.join();
-  auto const after = std::chrono::high_resolution_clock::now();
+  for (auto& th : threads) th.join();
+  auto ended_time = std::chrono::high_resolution_clock::now();
+  std::cout << green "Original string size: ` " yellow << target_string.size()
+            << green " `\nOriginal string bytes: ` " yellow;
+  for (auto item : target_string) std::cout << (uint16_t)item << " ";
+  std::cout << green " `\nOriginal string: ` " yellow;
+  for (auto item : target_string)
+    if (isgraph(item)) std::cout << item; else std::cout << ".";
+  std::cout << green " `\nResult suffix: ` " yellow;
+  union {
+    uint8_t bytes[8];
+    uint64_t big;
+  } res;
+  res.big = result;
+  for (auto item : res.bytes) std::cout << (uint16_t)item << " ";
+  std::cout << green "`\nResult hash: ` " yellow << std::bitset<64>(result_hash)
+            << green " `\nTime spent: ` " yellow
+            << duration_cast<std::chrono::milliseconds>(ended_time -
+                                                        started_time)
+                   .count()
+            << "ms" green " `\n";
 
-  auto const final_inside = (double)inside.load(std::memory_order_relaxed);
-  auto const final_ratio = final_inside / (double)total;
-  auto const final_area = final_ratio * radiussq * 4;
-  auto const expected_area = M_PI * radiussq;
-  auto const absolute_error = std::abs(expected_area - final_area);
-  auto const relative_error = absolute_error / expected_area * 100;
-  auto const time =
-      std::chrono::duration_cast<std::chrono::milliseconds>(after - before)
-          .count();
-
-  std::wprintf(
-      LR"(
-
-      Amount of points ended up inside circle: %13lu
-      Total amount of points:                  %13lu
-      Elapsed time:                            %13lu ms
-      Ratio:                                   %13.7lf
-      Calculated area:                         %13.7lf
-      Expected area:                           %13.7lf
-      Absolute error:                          %13.7lf
-      Relative error:                          %13.7lf %%
-
-)",
-      (size_t)final_inside, total, time, final_ratio, final_area, expected_area,
-      absolute_error, relative_error);
   return 0;
+}
+
+bool check_hash(size_t hash, size_t Z) {
+  const size_t mask = ((1ul << Z) - 1ul) << (sizeof(size_t) * 8 - Z);
+  return (hash & mask) == 0;
+}
+
+template <std::integral T>
+inline void hash_combine(std::size_t& seed, const T& v) {
+  thread_local static std::hash<T> hasher;
+  seed ^= hasher(v) + 0x9e3779b9 + (seed << 6ul) + (seed >> 2ul);
+}
+
+string gen_random_string(size_t const size, std::mt19937& mt) {
+  static std::uniform_int_distribution<uint8_t> distr{0, 255};
+  string ss;
+  ss.reserve(size);
+  for (size_t i = 0; i < size; ++i) {
+    ss.emplace_back(distr(mt));
+  }
+  return ss;
+}
+
+template <typename T>
+T input_checked(char const* string) {
+  T result;
+  std::stringstream ss;
+  ss << string;
+  ss >> result;
+  if (!ss.eof()) {
+    std::cerr << red "Invalid argument value\n" reset;
+    usage();
+  }
+  return result;
+}
+
+void usage() {
+  assert(ARGC > 0);
+  std::cerr << "Usage:\n\t " << ARGV[0]
+            << " <target number of zeros> <number of threads>\n";
+  exit(1);
 }
